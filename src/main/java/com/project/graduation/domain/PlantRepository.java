1f85bf7fa6bad013f9c41cd440457784ac748ebe
taeho <92752011+xogh3198@users.noreply.github.com>
2026-04-20 23:32:03 +0900

    Delete src/main/java/com/project/graduation directory

--- src/main/java/com/project/graduation/domain/PlantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.graduation.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PlantRepository extends JpaRepository<Plant, Long> {
-    // 유저 ID로 내 식물 목록 찾기
-    List<Plant> findByUserId(Long userId);
-}
