0a6e1eb4372791eff94821ca9631aaaa4f2c9b7a
xogh3198 <taeho3198@naver.com>
2026-05-17 21:52:30 +0900

    재배포

--- src/main/java/com/project/graduation/GraduationApplication.java
+++ src/main/java/com/project/graduation/GraduationApplication.java
@@ -3,7 +3,7 @@ package com.project.graduation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //spring
 public class GraduationApplication {
 
     public static void main(String[] args) {
